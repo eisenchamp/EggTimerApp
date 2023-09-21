e3a39146e7897960f59d12d7c774ae456b940ced
Eisenchamp <ttursynbekov07@gmail.com>
2023-09-21 17:13:15 +0600

    Egg Timer Final

--- EggTimerApp/ViewController.swift
+++ EggTimerApp/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimer = ["Soft" : 5, "Medium" : 420, "Hard" : 720]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        progressBar.transform = progressBar.transform.scaledBy(x: 1, y: 8)
+        progressBar.layer.cornerRadius = 10
+        progressBar.clipsToBounds = true
+        progressBar.layer.sublayers![1].cornerRadius = 10
+        progressBar.subviews[1].clipsToBounds = true
     }
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        totalTime = eggTimer[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer(){
+        if secondsPassed < totalTime{
+            secondsPassed += 1
+            let percentage = Float(secondsPassed)/Float(totalTime)
+            progressBar.progress = Float(percentage)
+        }else{
+            timer.invalidate()
+            titleLabel.text = "Egg is DONE!"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+                        player = try! AVAudioPlayer(contentsOf: url!)
+                        player.play()
+        }
+    }
+    
 }
 
